4b63fa04f209e936e590f04acb7410589f05e368
Pritam <ghoshpritam0708@gmail.com>
2026-07-16 11:59:44 +0530

    make a separate frontend folder

--- src/mailer/send_mail.js
+++ src/mailer/send_mail.js
@@ -14,4 +14,8 @@ export const transporter = nodemailer.createTransport({
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
+    tls: {
+        rejectUnauthorized: false
+    },
+    family: 4
 });
